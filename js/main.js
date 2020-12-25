4267d91c9f22b9571c93e91000a9d1ebeb85ba13
Sergey-Karpenok <Shamanito@rambler.ru>
2020-12-25 11:48:20 +0300

    update: fix mistake of bem tree

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $('.navbar__menu-button').on('click', function() {
 
-        $('.navbar-wrapper').toggleClass("navbar-wrapper--visible");
+        $('.navbar').toggleClass("navbar--visible");
     });
 
 
